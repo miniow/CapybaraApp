149b4c9be51a342640c9deba0335ce3eb18b925d
Michał Wnorowski <82973@student.pb.edu.pl>
2024-01-07 20:30:30 +0100

    add alarm layout

--- app/src/main/java/com/example/capybaraapp/AddAlarmActivity.java
+++ app/src/main/java/com/example/capybaraapp/AddAlarmActivity.java
@@ -2,13 +2,54 @@ package com.example.capybaraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 
-public class AddAlarm extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
+public class AddAlarmActivity extends AppCompatActivity {
+
+    private final Calendar calendar = Calendar.getInstance();
+    private ImageButton dateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
+
+        dateButton = findViewById(R.id.alarm_date);
+        dateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final int year = calendar.get(Calendar.YEAR);
+                final int month = calendar.get(Calendar.MONTH);
+                final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // Create a new instance of DatePickerDialog and show it
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddAlarmActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                // The user picked a date. You can use this date to set the alarm.
+                                // The month value is 0-based, so you might want to add 1.
+                                calendar.set(Calendar.YEAR, year);
+                                calendar.set(Calendar.MONTH, monthOfYear);
+                                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                                // Format the date and display it on the TextView, if you have one.
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMM d, yyyy", Locale.getDefault());
+                                String selectedDate = dateFormat.format(calendar.getTime());
+                                // Assuming you have a TextView to display the date
+                                // textViewDate.setText(selectedDate);
+                            }
+                        }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/capybaraapp/MainActivity.java
+++ app/src/main/java/com/example/capybaraapp/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.capybaraapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean nextAlarmVisible=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.alarm_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.show_next_alarm:
+
+                if(nextAlarmVisible){
+                    item.setIcon(R.drawable.ic_menu_hide_next_alarm);
+                    Toast.makeText(this, "sdfg", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    item.setIcon(R.drawable.ic_menu_show_next_alarm);
+                    Toast.makeText(this, "sdfg", Toast.LENGTH_SHORT).show();}
+                nextAlarmVisible= !nextAlarmVisible;
+                return true;
+            case R.id.new_alarm:
+                Intent intent = new Intent(this, AddAlarmActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
